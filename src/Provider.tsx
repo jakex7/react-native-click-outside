d04c60640514febcb3b1795eb3631b1c671b279f
mattgle.eth <44204622+mattgle@users.noreply.github.com>
2023-05-17 18:37:40 -0300

    Update src/Provider.tsx
    Co-authored-by: Jakub Grzywacz <jaqbgrzy@gmail.com>

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -6,7 +6,7 @@ import { isInRange } from './utils/helpers';
 type ClickOutsideProviderProps = {
   activateOnSwipe?: boolean;
   swipeThreshold?: number;
-  children: JSX.Element;
+  children: React.ReactNode;
 };
 
 let touchX: number | undefined;
